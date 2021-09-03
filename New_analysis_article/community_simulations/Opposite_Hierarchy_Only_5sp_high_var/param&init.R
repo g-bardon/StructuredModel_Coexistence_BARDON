232298e4c69b97d90aab4f9dcae9762754a4bc98
Gael Bardon <gael.bardon2@gmail.com>
2021-09-03 14:51:05 +0200

    "Added comments"

--- New_analysis_article/community_simulations/Opposite_Hierarchy_Only_5sp_high_var/param&init.R
+++ New_analysis_article/community_simulations/Opposite_Hierarchy_Only_5sp_high_var/param&init.R
@@ -6,34 +6,34 @@ init <- function(S){
 }
 
 parametrage <- function (S){
-  fert = rlnorm(S, meanlog = 3, sdlog=0.5)  ## loi log normale pour la fertilité, assez petite variance sinon très grosse fertilité possibles
-  mat_rate = rbeta(S, 5,5) ## Loi beta, distribution piquée centrée en 8/13
-  juv_surv = rbeta(S, 5,5) ## Loi beta, distribution piquée centrée en 8/13
-  adult_surv = rbeta(S, 5,5) ## Loi beta, distribution piquée centrée en 8/13
+  fert = rlnorm(S, meanlog = 3, sdlog=0.5)  ## log-normal distribution for fertility, low variance to avoid too large fertility
+  mat_rate = rbeta(S, 5,5) ## beta distribution centered in 1/2
+  juv_surv = rbeta(S, 5,5) ## beta distribution centered in 1/2
+  adult_surv = rbeta(S, 5,5) ## beta distribution centered in 1/2
   
-  alpha_intra = rnorm(S, 0.05, 0.01)  # Tirage des coef de competition intra selon loi normale 
+  alpha_intra = rnorm(S, 0.05, 0.01)  # Normal distribution for competition coefficients 
   beta_intra = rnorm(S, 0.05, 0.01)
   sig=0.02
   alpha_beta_inter = rmvnorm(n=S*(S-1), mean=c(0.04,0.04), sigma=matrix(c(sig^2, -(sig^2)*0.9, -(sig^2)*0.9, sig^2), 2,2, byrow = TRUE)) 
-  alpha=matrix(0,S,S)   ## Matrice de tous les coef de competition alpha : colonne i correspond à la competition exercée sur l'espèce i
-  beta=matrix(0,S,S)  ## Matrice de tous les coef de competition beta : colonne i correspond à la competition exercée sur l'espèce i
+  alpha=matrix(0,S,S)   ## Matrix of alpha competition coefficients : i column corresponds to the alpha competition undergoes by species i
+  beta=matrix(0,S,S)  ## Matrix of beta competition coefficients : i column corresponds to the beta competition undergoes by species i
   
   c=1
   for (i in 1:S){
     for (j in 1:S){
       if (i==j){
-        alpha[i,j] <- alpha_intra[i]  ## Coef de compet intra sur la diag
+        alpha[i,j] <- alpha_intra[i]  ## intraspecific competition coefficients on the diagonal
         beta[i,j] <- beta_intra[i]
       }else{
         x <- alpha_beta_inter[c,] 
-        alpha[i,j] <- x[1] ## Coef de compet inter dans le reste de la matrice, tiré dans le pool de coef alpha_beta_inter
+        alpha[i,j] <- x[1] ## alpha and beta interspecific competition coefficient with negative correlation
         beta[i,j] <- x[2]
         c=c+1
       }
     }
   }
   
-  return(list(fert, mat_rate, juv_surv, adult_surv, alpha, beta))   ## Retourne le jeu de parametre avec de la correlation entre alpha ij et beta ij
+  return(list(fert, mat_rate, juv_surv, adult_surv, alpha, beta))   ## Return the parameter set with the negative correlation structure
 }
 
 S=5
